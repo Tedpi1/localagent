36d2f084389ea9e212a66487de96c776d35c2ffc
Tedpi1 <moroted16@gmail.com>
2025-06-20 08:10:36 +0300

    new local AI Agent

---
+++ main.py
@@ -0,0 +1,20 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model=OllamaLLM(
+    model="llama3.2",
+)
+
+template="""
+Your are an Expert in Answereing Computer related questions.
+Here are some relevant reviews{reviews}
+here is the question to answer{question}
+
+"""
+
+prompt=ChatPromptTemplate.from_template(template)
+
+chain=prompt | model
+
+result=chain.invoke({"reviews": [], "question": "What is the best desktop computer for building AI agents?"})
+print(result)
