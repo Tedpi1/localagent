0602fe0066e2213450c351a9132ccbda0e7df973
Tedpi1 <moroted16@gmail.com>
2025-06-24 06:09:33 +0300

    teddy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-from vectory import retriever
+from vector import retriever
 
 model=OllamaLLM(
     model="llama3.2",
@@ -14,8 +14,15 @@ here is the question to answer{question}
 """
 
 prompt=ChatPromptTemplate.from_template(template)
-
 chain=prompt | model
 
-result=chain.invoke({"reviews": [], "question": "What is the best desktop computer for building AI agents?"})
-print(result)
+while True:
+    print("\n\n-------------------------")
+    question=input("Enter your question (q to quit): ")
+    if question.lower() == "q":
+        break
+    reviews=retriever.invoke(question)
+    
+
+    result=chain.invoke({"reviews": reviews, "question": question})
+    print(result)
